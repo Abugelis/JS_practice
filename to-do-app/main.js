6187c5ef0b68d9906593544c30585a339d511dc2
Edvinas Grotuzas <113600729+Abugelis@users.noreply.github.com>
2026-05-22 02:07:32 +0100

    Add delete task function

--- to-do-app/main.js
+++ to-do-app/main.js
@@ -2,15 +2,17 @@
 let taskInput = document.getElementById("task-input");
 // initialize button to add task
 let addTaskBtn = document.getElementById("add-task-btn");
+// initialize task display element
+let taskDisplay = document.getElementById("display-tasks");
 
 // Initialize task array 
-const tasks = []; 
+let tasks = []; 
 
 // Add even listener for the button, append task object to task array       
 addTaskBtn.addEventListener("click", () => {
     tasks.push(createTask());
-    
-    console.log(tasks);
+    taskInput.value = "";
+    displayTasks()
 });
 
 
@@ -21,8 +23,37 @@ function createTask() {
         taskName : taskInput.value,
         taskCompleted : false
     }
-
     return task;
-};
+}
 
+// clear element that displays tasks. loop through tasks and add them to dom along with the button to delete task
+function displayTasks(){
+    taskDisplay.innerHTML  = "";
 
+    tasks.forEach((task)=>{
+        let taskRow = document.createElement("div");
+        let para = document.createElement("p");
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.value = task.taskId;
+        
+        para.append(task.taskName);
+        
+        taskDisplay.appendChild(taskRow);
+
+        taskRow.appendChild(para)
+        taskRow.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener("click", ()=>{
+            deleteTask(task.taskId);
+        });
+    });
+}
+
+// delete task. filter out task based on id and replace tasks with new filtered out array
+function deleteTask(taskId) {
+    let newTasks = tasks.filter((task)=> task.taskId !== taskId);
+    tasks = newTasks;
+    taskDisplay.innerHTML = "";
+    displayTasks();
+}
